136739a360ba7e7ee2d8930f80c6a0747bf6ca33
Cosmo Colman <cosmo.colman@kcl.ac.uk>
2022-10-03 11:33:57 +0100

    Complete task

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
+import math
+
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise Exception(ValueError)
+    else:
+        list = []
+
+        num = 2
+        while len(list) < number_of_primes:
+            if isPrime(num):
+                list.append(num)
+            num += 1
+
+        return list
+
+def isPrime(num):
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
